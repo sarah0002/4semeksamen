7d5ce0e31f9b9d3c910e17031757f7487ede3192
Signe <sila0001@stud.kea.dk>
2025-05-27 10:37:05 +0200

    links i knapper

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import alpinejs from "@astrojs/alpinejs";
@@ -6,8 +6,4 @@ import alpinejs from "@astrojs/alpinejs";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
-  experimental: {
-    inlineStyles: 'always', 
-  },
 });
-
